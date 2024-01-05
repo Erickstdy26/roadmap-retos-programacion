a398c0efe1aea7ce6508b8c10b437f77cc63dab9
erick <erickbolivarstdy@outlook.com>
2024-01-04 21:25:51 -0300

    #01 - PYTHON

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/erickstdy26.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/erickstdy26.py
@@ -136,3 +136,6 @@ print("Opcional --------------------------------------------------")
 for numero in range(10, 56):
     if numero % 2 == 0 and numero != 16 and numero % 3 != 0:
         print(numero)
+    
+#segundo intento de pull request
+        
